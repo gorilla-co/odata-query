d7f38d69a9845c9dee43e7817527f70cc2d5cb59
Oliver Hofkens <oliver@gorilla.co>
2021-05-28 16:34:07 +0200

    Docs; Add quickstart, include README in index, describe lexer and parser.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = "OData Query"
-copyright = "2021, Gorilla"
+copyright = "2021, Gorillini NV"
 author = "Oliver Hofkens"
 
 # The full version, including alpha/beta/rc tags
@@ -30,7 +30,13 @@ release = "0.4.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.doctest",
+    "sphinx.ext.graphviz",
+    "myst_parser",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
