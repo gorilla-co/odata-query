ce2bb237826a807d5682956844c7a98c47fc50f8
Andres Zelcer <andres@flin.co>
2022-10-25 16:47:23 -0300

    Modify shorthand:  - Split `apply_odata_query` in two functions to ease rewriting  - Add function to create AST (and rewrite if desired)  - Add function to apply parsed AST, showing how to manage sqlalchemy's polymorphism

--- odata_query/sqlalchemy/shorthand.py
+++ odata_query/sqlalchemy/shorthand.py
@@ -1,9 +1,11 @@
-from typing import List
+from typing import List, Optional, Dict
 
 from sqlalchemy.sql.expression import ClauseElement, Select
+from sqlalchemy.orm.attributes import InstrumentedAttribute
 
+import sqlalchemy
 from odata_query.grammar import ODataLexer, ODataParser  # type: ignore
-
+from odata_query.rewrite import AliasRewriter
 from .sqlalchemy_clause import AstToSqlAlchemyClauseVisitor
 
 
@@ -11,18 +13,27 @@ def _get_joined_attrs(query: Select) -> List[str]:
     return [str(join[0]) for join in query._setup_joins]
 
 
-def create_odata_ast(odata_query: str) -> ClauseElement:
+def create_odata_ast(
+    odata_query: str,
+    name_entity_map: Optional[Dict[str, InstrumentedAttribute]] = None,
+) -> ClauseElement:
     """
     Create an AST for a query
 
     Args:
         odata_query: OData query string.
+        name_entity_map: Dict for rewriting, optional
     Returns:
         AST
     """
     lexer = ODataLexer()
     parser = ODataParser()
     ast = parser.parse(lexer.tokenize(odata_query))
+    if name_entity_map:
+        rewriter = AliasRewriter(
+            {alias: str(field) for alias, field in name_entity_map.items()}
+        )
+        ast = rewriter.visit(ast)
     return ast
 
 
@@ -35,7 +46,20 @@ def apply_ast_query(query: ClauseElement, ast) -> ClauseElement:
     Returns:
         ClauseElement: The modified query
     """
-    model = [col["entity"] for col in query.column_descriptions]
+    model = [
+        col["entity"] for col in query.column_descriptions if col["entity"] is not None
+    ]
+
+    for col in query.column_descriptions:
+        if col["aliased"]:
+            aliased_insp = col["entity"]._aliased_insp
+            if aliased_insp._is_with_polymorphic:
+                model.extend(
+                    [
+                        getattr(col["entity"], sub_aliased_insp.class_.__name__)
+                        for sub_aliased_insp in aliased_insp._with_polymorphic_entities
+                    ]
+                )
 
     transformer = AstToSqlAlchemyClauseVisitor(model)
     where_clause = transformer.visit(ast)
