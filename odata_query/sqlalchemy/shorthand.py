9fef2c9b841a0496e99cc4aa4c84248a8a67e1c6
Oliver Hofkens <oliver@gorilla.co>
2022-12-19 13:54:22 +0100

    fix(sqlalchemy): return a 1.x `Query` if a 1.x `Query` is passed
    fixes: #34

--- odata_query/sqlalchemy/shorthand.py
+++ odata_query/sqlalchemy/shorthand.py
@@ -26,12 +26,15 @@ def apply_odata_query(query: ClauseElement, odata_query: str) -> ClauseElement:
     lexer = ODataLexer()
     parser = ODataParser()
 
+    clause_elem: ClauseElement
     if isinstance(query, Query):
         # For now, we keep supporting the 1.x style of queries unofficially.
         # GITHUB-34
-        query = query.__clause_element__()
+        clause_elem = query.__clause_element__()
+    else:
+        clause_elem = query
 
-    model = query.columns_clause_froms[0].entity_namespace
+    model = clause_elem.columns_clause_froms[0].entity_namespace
 
     ast = parser.parse(lexer.tokenize(odata_query))
     transformer = AstToSqlAlchemyOrmVisitor(model)
