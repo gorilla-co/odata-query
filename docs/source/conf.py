e7f63f0b19112161f887b379b73ec90c4b03a22d
Oliver Hofkens <oliver@gorilla.co>
2023-02-17 09:45:58 +0100

    Bump version: 0.8.0 → 0.8.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Gorillini NV"
 author = "Oliver Hofkens"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.0"
+release = "0.8.1"
 
 
 # -- General configuration ---------------------------------------------------
