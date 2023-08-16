33d7afc89a69e97e659dcaa21ea8f7347e5668d6
Byron Thomas <byron.thomas@gmail.com>
2023-08-16 17:26:58 +0100

    Avoid clashing by base functions by setting odata

--- odata_query/sqlalchemy/functions_ext.py
+++ odata_query/sqlalchemy/functions_ext.py
@@ -4,35 +4,44 @@ from sqlalchemy.types import Integer, String
 
 class strpos(GenericFunction):
     type = Integer
+    package = "odata"
 
 
 class substr(GenericFunction):
     type = String
+    package = "odata"
 
 
 class lower(GenericFunction):
     type = String
+    package = "odata"
 
 
 class upper(GenericFunction):
     type = String
+    package = "odata"
 
 
 class ltrim(GenericFunction):
     type = String
+    package = "odata"
 
 
 class rtrim(GenericFunction):
     type = String
+    package = "odata"
 
 
 class ceil(GenericFunction):
     type = Integer
+    package = "odata"
 
 
 class floor(GenericFunction):
     type = Integer
+    package = "odata"
 
 
 class round(GenericFunction):
     type = Integer
+    package = "odata"
