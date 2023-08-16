51e27399782fa47ca1c11f039e508f96a9050f84
Byron Thomas <byron.thomas@gmail.com>
2023-08-16 17:27:30 +0100

    Allow cache inheritance on the functions we define for sqlalchemy

--- odata_query/sqlalchemy/functions_ext.py
+++ odata_query/sqlalchemy/functions_ext.py
@@ -5,43 +5,52 @@ from sqlalchemy.types import Integer, String
 class strpos(GenericFunction):
     type = Integer
     package = "odata"
+    inherit_cache = True
 
 
 class substr(GenericFunction):
     type = String
     package = "odata"
+    inherit_cache = True
 
 
 class lower(GenericFunction):
     type = String
     package = "odata"
+    inherit_cache = True
 
 
 class upper(GenericFunction):
     type = String
     package = "odata"
+    inherit_cache = True
 
 
 class ltrim(GenericFunction):
     type = String
     package = "odata"
+    inherit_cache = True
 
 
 class rtrim(GenericFunction):
     type = String
     package = "odata"
+    inherit_cache = True
 
 
 class ceil(GenericFunction):
     type = Integer
     package = "odata"
+    inherit_cache = True
 
 
 class floor(GenericFunction):
     type = Integer
     package = "odata"
+    inherit_cache = True
 
 
 class round(GenericFunction):
     type = Integer
     package = "odata"
+    inherit_cache = True
