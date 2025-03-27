ed5d7fc111e8395bff4cfbdace7d9669f7544dd3
Dmitrii <tokarenko@outlook.com>
2025-03-27 21:52:00 +0300

    Add query options support for SQLAlchemy: orderby, top, skip

--- odata_query/sqlalchemy/shorthand.py
+++ odata_query/sqlalchemy/shorthand.py
@@ -1,12 +1,72 @@
-from typing import List
+import re
+from typing import List, Literal
 
+from odata_query.grammar import ODataLexer, ODataParser  # type: ignore
 from sqlalchemy.orm.query import Query
 from sqlalchemy.sql.expression import ClauseElement, Select
 
-from odata_query.grammar import ODataLexer, ODataParser  # type: ignore
-
 from .core import AstToSqlAlchemyCoreVisitor
 from .orm import AstToSqlAlchemyOrmVisitor
+
+
+QueryOptions = dict[
+    Literal["filter", "count", "orderby", "top", "skip", "search", "format"],
+    str,
+]
+
+query_option_names = set(
+    [
+        "filter",
+        "count",
+        "orderby",
+        "top",
+        "skip",
+        "search",
+        "format",
+    ]
+)
+
+
+def _get_query_options(odata_query: str) -> QueryOptions:
+    """Get system query options as per odata specification"""
+
+    query_options = {}
+
+    for component in odata_query.split("$"):
+        component_name = None
+        match = re.match("(\w+)=", component)
+        if match:
+            component_name = match.group(1)
+            if component_name not in query_option_names:
+                raise ValueError(f"Unknown option name {component_name}")
+            query_options[component_name] = component[len(match.group()) :]
+        else:
+            query_options["filter"] = component
+
+    return query_options
+
+
+def _apply_query_options(
+    query_options: QueryOptions, where_clause: ClauseElement
+) -> ClauseElement:
+    """Applies OData query options to a SQLAlchemy query."""
+
+    # Apply orderby system query option.
+    orderby = query_options.get("orderby", None)
+    if orderby:
+        where_clause = where_clause.order_by(orderby)
+
+    # Apply top and skip system query options.
+    top = query_options.get("top", None)
+    skip = query_options.get("skip", None)
+    if top and skip:
+        top = int(top)
+        skip = int(skip)
+        where_clause = where_clause.limit(top).offset(skip * top)
+    elif top:
+        where_clause = where_clause.limit(int(top))
+
+    return where_clause
 
 
 def _get_joined_attrs(query: Select) -> List[str]:
@@ -40,7 +100,8 @@ def apply_odata_query(query: ClauseElement, odata_query: str) -> ClauseElement:
 
     model = clause_elem.columns_clause_froms[0].entity_namespace
 
-    ast = parser.parse(lexer.tokenize(odata_query))
+    query_options = _get_query_options(odata_query)
+    ast = parser.parse(lexer.tokenize(query_options["filter"]))
     transformer = AstToSqlAlchemyOrmVisitor(model)
     where_clause = transformer.visit(ast)
 
@@ -52,7 +113,9 @@ def apply_odata_query(query: ClauseElement, odata_query: str) -> ClauseElement:
         ):
             query = query.join(required_join)
 
-    return query.filter(where_clause)
+    query = query.filter(where_clause)
+    query = _apply_query_options(query_options, query)
+    return query
 
 
 def apply_odata_core(query: ClauseElement, odata_query: str) -> ClauseElement:
@@ -69,7 +132,11 @@ def apply_odata_core(query: ClauseElement, odata_query: str) -> ClauseElement:
     parser = ODataParser()
     table = query.columns_clause_froms[0]
 
-    ast = parser.parse(lexer.tokenize(odata_query))
+    query_options = _get_query_options(odata_query)
+    ast = parser.parse(lexer.tokenize(query_options["filter"]))
     transformer = AstToSqlAlchemyCoreVisitor(table)
     where_clause = transformer.visit(ast)
-    return query.filter(where_clause)
+
+    query = query.filter(where_clause)
+    query = _apply_query_options(query_options, query)
+    return query
